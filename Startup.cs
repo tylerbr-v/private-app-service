492cea4badd4a29b766b3d3e3197be875361c3ce
Tyler Branscombe <tylerbr-v@herbalife.com>
2024-08-29 17:53:31 +0000

    remove azure bot

--- Startup.cs
+++ Startup.cs
@@ -3,13 +3,9 @@
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-using Microsoft.BotBuilderSamples.Bots;
 using Microsoft.Extensions.Hosting;
 
 namespace Microsoft.BotBuilderSamples
@@ -26,27 +22,7 @@ namespace Microsoft.BotBuilderSamples
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            var storage = new MemoryStorage();
-            var conversationState = new ConversationState(storage);
-            services.AddSingleton(conversationState);
-            
-            services.AddHttpClient().AddControllers().AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.MaxDepth = HttpHelper.BotMessageSerializerSettings.MaxDepth;
-            });
-
-            // Create the Bot Framework Authentication to be used with the Bot Adapter.
-            services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
-
-            // Create the Bot Adapter with error handling enabled.
-            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
-
-            services.AddSingleton<BotStateService>();
-
-            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, HerbalifePOC>();
-
+            services.AddHttpClient().AddControllers().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
